f4eeed3aa67aefdeb14b8bda6a7b532c6795e023
Sascha Lüdecke <sascha@meta-x.de>
2023-07-31 00:10:30 +0200

    Support for Heroic 2.9.x for GOG Games
    . tested with Dead Cells (Linux native) and Riftbreaker (Windows/proton)

--- src/wrap.rs
+++ src/wrap.rs
@@ -6,7 +6,11 @@ mod legendary;
 pub fn get_game_name_from_heroic_launch_commands(roots: &[RootsConfig], commands: &[String]) -> Result<String, Error> {
     // TODO.2023-07-19 check only applicable roots
 
-    let parsers = vec![gogdl::parse_heroic_2_8, legendary::parse_heroic_2_8];
+    let parsers = vec![
+        gogdl::parse_heroic_2_9,
+        gogdl::parse_heroic_2_8,
+        legendary::parse_heroic_2_8,
+    ];
     match parsers.iter().find_map(|parser| parser(roots, commands)) {
         Some(game_name) => Ok(game_name),
         None => Err(Error::WrapCommandNotRecognized {
--- src/wrap/gogdl.rs
+++ src/wrap/gogdl.rs
@@ -1,55 +1,126 @@
+use std::{env, str::FromStr};
+
 use itertools::Itertools;
 
-use crate::{resource::config::RootsConfig, scan::launchers::heroic::get_gog_games_library};
+use crate::{
+    prelude::StrictPath,
+    resource::{config::RootsConfig, manifest::Store},
+    scan::launchers::heroic::get_gog_games_library,
+};
 
-// TODO.2023-07-30 - Linux Native: heroic 2.9.x changed invocation for gogdl
-// based games and we no longer get the GAME_DIR ang GAME_ID information from
-// the command line parameters.
-//
-// Instead we need to rely on GAMEDIR/goggame-GAME_ID.id (wine/proton) or
-// GAMEDIR/game/__game/goggame-GAME_ID.id (Linux native) to get a game id
-//
-// Linux Native:
-//
-// commands is GAME_DIR/start.sh
-//
-// Wine/Proton:
-//
-// check environment variable
-// STEAM_COMPAT_INSTALL_PATH='/home/saschal/Games/The Riftbreaker'
+/// Parsing of command line for Heroic 2.9.x
+pub fn parse_heroic_2_9(roots: &[RootsConfig], commands: &[String]) -> Option<String> {
+    let mut game_id = String::default();
+
+    // Is this a Linux native game?
+    // Try checking dirname(commands[0])/game/__game/goggame-GAME_ID.id
+    let id_files = StrictPath::from(commands[0].as_str())
+        .parent_if_file()
+        .joined("game")
+        .joined("__game")
+        .joined("goggame-*.id")
+        .glob();
+    if id_files.len() == 1 {
+        let id_file = id_files[0].as_std_path_buf();
+        log::debug!(
+            "HeroicGogdl::parse_heroic_2_9: found Linux native goggame-*.id: {:?}",
+            id_file
+        );
+        game_id = String::from_str(
+            id_file
+                .file_stem()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .strip_prefix("goggame-")
+                .unwrap(),
+        )
+        .unwrap();
+    }
+
+    if game_id.is_empty() {
+        // Is this a Windows game?
+        //
+        // check environment variable
+        // STEAM_COMPAT_INSTALL_PATH='/home/MYUSER/Games/The Riftbreaker' and
+        // then env(STEAM_COMPAT_INSTALL_PATH)/goggame-GAME_ID.id
+        if let Ok(env_install_path) = env::var("STEAM_COMPAT_INSTALL_PATH") {
+            let id_files = StrictPath::from(env_install_path.as_str())
+                .joined("goggame-*.id")
+                .glob();
+            if id_files.len() == 1 {
+                let id_file = id_files[0].as_std_path_buf();
+                log::debug!(
+                    "HeroicGogdl::parse_heroic_2_9: found Windows goggame-*.id: {:?}",
+                    id_file
+                );
+                game_id = String::from_str(
+                    id_file
+                        .file_stem()
+                        .unwrap()
+                        .to_str()
+                        .unwrap()
+                        .strip_prefix("goggame-")
+                        .unwrap(),
+                )
+                .unwrap();
+            }
+        }
+    }
+
+    if game_id.is_empty() {
+        None
+    } else {
+        roots
+            .iter()
+            .filter(|root| root.store == Store::Heroic)
+            .find_map(|root| {
+                log::debug!("HeroicGogdl::parse_heroic_2_9: checking root {:?}", root);
+                match get_gog_games_library(root) {
+                    Some(gog_games) => gog_games.iter().find_map(|g| match g.app_name == game_id {
+                        true => Some(g.title.clone()),
+                        false => None,
+                    }),
+                    None => None,
+                }
+            })
+    }
+}
 
 /// Parsing of command line for Heroic 2.8.x (and probably earlier versions)
 pub fn parse_heroic_2_8(roots: &[RootsConfig], commands: &[String]) -> Option<String> {
     let mut iter = commands.iter();
 
     if iter.find_position(|p| p.ends_with("gogdl")).is_none() {
-        log::debug!("HeroicGogdl::parse: gogdl not found");
+        log::debug!("HeroicGogdl::parse_heroic_2_8: gogdl not found");
         return None;
     }
-    log::debug!("HeroicGogdl::parse: gogdl found");
+    log::debug!("HeroicGogdl::parse_heroic_2_8: gogdl found");
 
     if iter.find_position(|p| p.ends_with("launch")).is_none() {
-        log::debug!("HeroicGogdl::parse: launch not found");
+        log::debug!("HeroicGogdl::parse_heroic_2_8: launch not found");
         return None;
     }
     // TODO.2023-07-19 fails if user selects different game exe in heroic
     let game_dir = iter.next().unwrap();
     let game_id = iter.next().unwrap();
     log::debug!(
-        "HeroicGogdl::parse: gogdl launch found: dir = {}, id = {}",
+        "HeroicGogdl::parse_heroic_2_8: gogdl launch found: dir = {}, id = {}",
         game_dir,
         game_id
     );
 
-    // TODO.2023-07-14 filter for root.type = Heroic
-    roots.iter().find_map(|root| {
-        log::debug!("HeroicGogdl::parse: checking root {:?}", root);
-        match get_gog_games_library(root) {
-            Some(gog_games) => gog_games.iter().find_map(|g| match g.app_name == *game_id {
-                true => Some(g.title.clone()),
-                false => None,
-            }),
-            None => None,
-        }
-    })
+    roots
+        .iter()
+        .filter(|root| root.store == Store::Heroic)
+        .find_map(|root| {
+            log::debug!("HeroicGogdl::parse_heroic_2_8: checking root {:?}", root);
+            match get_gog_games_library(root) {
+                Some(gog_games) => gog_games.iter().find_map(|g| match g.app_name == *game_id {
+                    true => Some(g.title.clone()),
+                    false => None,
+                }),
+                None => None,
+            }
+        })
 }
